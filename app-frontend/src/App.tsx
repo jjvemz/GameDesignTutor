d368c78353fcf861b82d4ae20564fc1f0e98e1cb
jjvemz <juanj.vergaram@gmail.com>
2025-06-25 12:02:06 -0400

    Routing, header and footer initialized

--- app-frontend/src/App.tsx
+++ app-frontend/src/App.tsx
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Tutor from "./pages/Tutor";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/about" element={<About />}/>
+          <Route path="/tutor" element={<Tutor />}/>
+          <Route path="/" element={<Home />}/>
+          <Route path="*"element={<NotFound />}/>
+        </Routes>
+    </Router>
   );
 }
 
