c5ff558cf60c43e8d298e4db114846b35c9fa0fa
jjvemz <juanj.vergaram@gmail.com>
2025-06-27 10:52:39 -0400

    Create Login and register pages

---
+++ app-frontend/src/Layout/Login/LoginLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/header'
+import Footer from '../../components/footer/footer'
+
+const LoginLayout = () => {
+  return (
+    <>
+        <Header/>
+        <Footer/>
+    </>
+  )
+}
+
+export default LoginLayout
